c4139d86824d8f5deea326cf0e27926a3c333dac
Yusadolat <yusadolat@gmail.com>
2018-11-03 15:14:59 +0100

    Portfolio Changes

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -21,8 +21,8 @@ class Portfolio extends React.Component {
 							<div className="container">
 								<div className="row center-xs">
 									<div className="HeaderPortfolio__titlewrap Page__titlewrap text-center col-xs-12 col-md-10 col-lg-7">
-										<h2 className="HeaderPortfolio__title Page__title">Portafolio</h2>
-										<p className="HeaderPortfolio__description Page__description">Un poco de mi trabajo</p>
+										<h2 className="HeaderPortfolio__title Page__title">Portfolio</h2>
+										<p className="HeaderPortfolio__description Page__description">A little bit of my work</p>
 									</div>
 								</div>
 							</div>
