6804fb354690ecf5c85e1d0ad622d1b68ed1da42
yusadolat <yusadolat@gmail.com>
2019-04-15 14:37:24 +0100

    Fix typo...

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,11 +20,11 @@ class About extends React.Component {
                 <div className="HeaderContact__titlewrap Page__titlewrap text-center col-xs-12 col-md-10 col-lg-7">
                   <h2 className="About__title Page__title"> About Me </h2>{" "}
                   <p className="About__description Page__description">
-                    My career in web development spans over 5 years. After years
+                    My career in web development spans over 4 years. After years
                     of working at agencies helping clients such as APTN News,
                     Canada, Qalam Edu Center, Somaliland, GirongVR and Evidence
                     Radio Nigeria bring engaging and effective experiences to
-                    the widest audience possible, I Founded Hostailor Inc where
+                    the widest possible audience , I Founded Hostailor Inc where
                     I help SME 's take charge of their hosting needs. I
                     currently Work with IderaOS(Ideraos helps you sell online
                     with ease) as a Front-end Engineer, I love seeking out
