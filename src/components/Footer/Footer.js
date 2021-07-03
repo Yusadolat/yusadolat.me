5e868141ab208e7b7e90229a36171f479a02a5de
Yusuf Adeyemo <yusadolat@gmail.com>
2021-07-03 05:39:50 +0000

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -73,7 +73,7 @@ export default props => (
       </Icon>
     </Social>
     <Copy>
-      © 2019 - All rights reserved. Made with <span>❤</span> by{" "}
+      © 2021 - All rights reserved. Made with <span>❤</span> by{" "}
       <a
         href="https://twitter.com/yusadolat"
         target="_blank"
